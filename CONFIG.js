8fab93642077db81d0f5bcacbe166005e9a803a9
zhangyunchuan <zhangyunchuan@csrd.cn>
2022-02-14 14:28:45 +0800

    1111111

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: " I Love You",
   descriptionHeader:
-    "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
-  buttonYes: "Đồng Ý",
-  buttonNo: "Không! Còn lâu nhé.",
-  titleModar: "Cảm ơn em vì đồng ý",
+      "你愿意做我女朋友吗？？？",
+  buttonYes: "我愿意",
+  buttonNo: "对不起，我们不合适",
+  titleModar: "我的姐姐",
   descriptionModar:
-    'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+      '从认识你那一刻，一直都是那么的快乐 </br> 我希望以后可以一直这么快乐 </br>',
 };
